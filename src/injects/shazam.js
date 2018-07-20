cf35a477714be1defbba91571182470454cd894a
Oleg <oleg.r646@gmail.com>
2018-07-20 22:53:26 +0300

    0.3.3 shazam download fixed

--- src/injects/shazam.js
+++ src/injects/shazam.js
@@ -9,14 +9,22 @@ $(document).ready(function () {
 
 function setButtons(){
     logme("buttons set");
-    $("article.shz-partial-track div.details").after("<button data-shz-beacon=\"type=btn,providerdesc=overflow-click\" class=\"ROYMusicHelperDownloadButton popup-btn\"><img alt='download' src='" + chrome.runtime.getURL("resources/images/general/download.png") + "'></button>");
+    $("article.shz-partial-track div.details").after("<button  class=\"ROYMusicHelperDownloadButton popup-btn\"><img alt='download' src='" + chrome.runtime.getURL("resources/images/general/download.png") + "'></button>");
+    //data-shz-beacon="type=btn,providerdesc=overflow-click"
     $(".ROYMusicHelperDownloadButton").click(function (event) {
         console.log("DOWNLOAD BUTTON CLICKED");
         event.stopImmediatePropagation();
 
         let detailsDOM = $(event.target).parent().children(".details");
-        let title = $(detailsDOM).find(".title a").text();
-        let author = $(detailsDOM).find(".artist a").text();
+        let title = $(detailsDOM).find(".title .ellip").text();
+        let author = $(detailsDOM).find(".artist .ellip").text();
+
+        console.log("event.target");
+        console.log(event.target);
+        console.log("detailsDOM: ");
+        console.log(detailsDOM);
+        console.log("title: " + title);
+        console.log("author: " + author);
 
         let text = title + " " + author;
         console.log("TEXT: " + text);
