9192dc9967d15c81c2f008106fcd2b738b118e81
Oleg <oleg.r646@gmail.com>
2018-07-21 16:42:12 +0300

    0.3.5 shazam complete 70%. Working on correcting first enter to page.

---
+++ src/injects/mp3party.net.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    console.log("READY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+    $(".download-btn-link").after($("<div>").addClass("download-btn-link").css("cursor", "pointer").click(function () {
+        let href = "http://mp3party.net" + $(this).parent().children(".name").children("a").attr("href");
+        chrome.runtime.sendMessage({action: "processSongPage", value: href, windowObject: "mp3partyNetSource"});
+    })).remove();
+});
+
+function setButtons(){
+    logme("buttons set");
+    $("article.shz-partial-track div.details").after("<button  class=\"ROYMusicHelperDownloadButton popup-btn\"><img alt='download' src='" + chrome.runtime.getURL("resources/images/general/download.png") + "'></button>");
+    //data-shz-beacon="type=btn,providerdesc=overflow-click"
+    $(".ROYMusicHelperDownloadButton").click(function (event) {
+        console.log("DOWNLOAD BUTTON CLICKED");
+        event.stopImmediatePropagation();
+
+        let detailsDOM = $(event.target).parent().children(".details");
+        let title = $(detailsDOM).find(".title .ellip").text();
+        let author = $(detailsDOM).find(".artist .ellip").text();
+
+        console.log("event.target");
+        console.log(event.target);
+        console.log("detailsDOM: ");
+        console.log(detailsDOM);
+        console.log("title: " + title);
+        console.log("author: " + author);
+
+        let text = title + " " + author;
+        console.log("TEXT: " + text);
+
+    });
+}
--- src/injects/shazam.js
+++ src/injects/shazam.js
@@ -1,47 +1,33 @@
+window.addEventListener('popstate', function () {
+    console.log("HREF CHANGED");
+});
 $(document).ready(function () {
     console.log("READY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(window.location.href);
+    console.log($(document).clone().get());
+
     $(".main").on("DOMSubtreeModified", function(event){
         $(".main").off("DOMSubtreeModified");
-        setButtons();
-        $(".shz-frame-myshazam-shazams").on("DOMSubtreeModified", function(e){logtarget("DOMSubtreeModified .active", e);});
+        $(".shz-frame-myshazam-shazams").on("DOMSubtreeModified", setButtons);
     });
 });
 
-function setButtons(){
-    logme("buttons set");
-    $("article.shz-partial-track div.details").after("<button  class=\"ROYMusicHelperDownloadButton popup-btn\"><img alt='download' src='" + chrome.runtime.getURL("resources/images/general/download.png") + "'></button>");
-    //data-shz-beacon="type=btn,providerdesc=overflow-click"
+let downloadButtonHTML = "<div  class=\"ROYMusicHelperDownloadButton popup-btn flex-reset\"><img alt='download' src='" + chrome.runtime.getURL("resources/images/general/download.png") + "'></div>";
+
+function setButtons(event){
+    if(event.target !== event.currentTarget){
+        return;
+    }
+    $("article.shz-partial-track div.details").after(downloadButtonHTML);
+
     $(".ROYMusicHelperDownloadButton").click(function (event) {
-        console.log("DOWNLOAD BUTTON CLICKED");
         event.stopImmediatePropagation();
 
-        let detailsDOM = $(event.target).parent().children(".details");
+        let detailsDOM = $(event.currentTarget).parent().children(".details");
         let title = $(detailsDOM).find(".title .ellip").text();
         let author = $(detailsDOM).find(".artist .ellip").text();
 
-        console.log("event.target");
-        console.log(event.target);
-        console.log("detailsDOM: ");
-        console.log(detailsDOM);
-        console.log("title: " + title);
-        console.log("author: " + author);
-
         let text = title + " " + author;
-        console.log("TEXT: " + text);
         chrome.runtime.sendMessage({action: "processTitle", value: text});
     });
-}
-
-function logme(text){
-    console.log("[EVENT]: " + text);
-}
-
-function logtarget(text, event){
-    // let classes = "";
-    // event.target.classList.
-    if(event.target === event.currentTarget){
-        setButtons();
-        console.log("[EVENT]: " + text + ": " + event.target.tagName + event.currentTarget + event.target.className);
-
-    }
 }
