72edf75b48f96a2d46153082c4a97bced24666c1
Oleg <oleg.r646@gmail.com>
2018-12-14 00:26:56 +0200

    0.6.1 add test for asyncController

--- src/test/smt/core/controller/asyncControllerSpec.js
+++ src/test/smt/core/controller/asyncControllerSpec.js
@@ -7,24 +7,28 @@ describe("asyncController spec", function () {
     const DUMMY_CLEANED_TITLE = "dctitle";
 
     let controller;
+
+    let textCleanerStub;
+    let sourceStub;
+
     let getSongListByTitleFunctionStub;
 
     beforeEach(function () {
-        let cleanFunctionStub = sinon.stub().withArgs(DUMMY_TITLE).returns(DUMMY_CLEANED_TITLE);
-        let textCleanerStub = {clean : cleanFunctionStub};
+        textCleanerStub = {clean : sinon.stub().withArgs(DUMMY_TITLE).returns(DUMMY_CLEANED_TITLE)};
 
-        getSongListByTitleFunctionStub = sinon.stub().withArgs(DUMMY_CLEANED_TITLE);
-        //todo accept only DUMMY_CLEANED_TITLE
-        let sourceStub = {getSongListByTitle : getSongListByTitleFunctionStub};
-        let getSupportedSourcesFunctionStub = sinon.stub().returns([sourceStub, sourceStub, sourceStub]);
-        let sourceManagerStub = {getSupportedSources : getSupportedSourcesFunctionStub};
-        controller = new Controller(textCleanerStub, sourceManagerStub);
-        console.log("BeforeEach main");
+        getSongListByTitleFunctionStub = sinon.stub();
+
+        sourceStub = {getSongListByTitle : getSongListByTitleFunctionStub};
     });
 
     it('should clean title and iterate over all sources', function () {
-        controller.processTitle(DUMMY_TITLE, null);
+        let SOURCE_RESPONSE_ACCUMULATOR = {};
+        let sourceManagerStub = {getSupportedSources : sinon.stub().returns([sourceStub, sourceStub, sourceStub])};
+        controller = new Controller(textCleanerStub, sourceManagerStub);
+
+        controller.processTitle(DUMMY_TITLE, SOURCE_RESPONSE_ACCUMULATOR);
         assert.equal(getSongListByTitleFunctionStub.callCount, 3);
+        assert.isTrue(getSongListByTitleFunctionStub.calledWithExactly(DUMMY_CLEANED_TITLE, SOURCE_RESPONSE_ACCUMULATOR));
     });
 
 });
