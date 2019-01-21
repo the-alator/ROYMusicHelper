26db79dd612b1f827c25a9d1f341837a918ea5ac
Oleg <oleg.r646@gmail.com>
2019-01-21 03:25:29 +0200

    йцу

--- src/main/extension/background/background.js
+++ src/main/extension/background/background.js
@@ -6,10 +6,6 @@ const smt = new Smt();
 
 log.debug("EXTENSION INSTALLED");
 
-chrome.tabs.create({url: DEBUG_HTML_PAGE});
-chrome.tabs.create({url: SOURCES_SPEC_HTML_PAGE});
-
-
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     log.debug("Message: " + JSON.stringify(request));
     switch (request.action){
--- src/main/extension/popup/debug.js
+++ src/main/extension/popup/debug.js
@@ -1,21 +1,5 @@
-let dzidzioMarsik = document.getElementById("Дзидзьо - марсик");
-let dzidzioSara = document.getElementById("дзидзьо сара");
-let introMantador = document.getElementById("Intro Matador/Perreo Japonés Dura DJ");
 let SAD = document.getElementById("SAD");
 let SADInput = document.getElementById("SADInput");
-
-dzidzioMarsik.addEventListener("click", function (e) {
-    chrome.runtime.sendMessage({action: "getSongListByTitle", value: dzidzioMarsik.value});
-});
-
-dzidzioSara.addEventListener("click", function (e) {
-    chrome.runtime.sendMessage({action: "getSongListByTitle", value: dzidzioSara.value});
-});
-
-
-introMantador.addEventListener("click", function (e) {
-    chrome.runtime.sendMessage({action: "getSongListByTitle", value: introMantador.value});
-});
 
 SAD.addEventListener("click", function (e) {
     console.log("attempt to SAD " + SADInput.value);
